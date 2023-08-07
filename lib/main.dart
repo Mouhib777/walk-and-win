e6b5be0d89a004429e4c2567043fa4522926bab5
Mouhib777 <mouhibsahbani@gmail.com>
2023-08-07 10:04:49 -0400

    c

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:walk_and_win/constant/constant.dart';
+import 'package:walk_and_win/firebase_options.dart';
 import 'package:walk_and_win/screens/splash.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async{
+    WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+  options: DefaultFirebaseOptions.currentPlatform,
+);
+
+  runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,27 +19,29 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+      Color myColor = primarycolor;
+
+    MaterialColor myThemeColor = MaterialColor(
+      myColor.value,
+      <int, Color>{
+        50: myColor.withOpacity(0.1), // Define shades for the color (50 to 900)
+        100: myColor.withOpacity(0.2),
+        200: myColor.withOpacity(0.3),
+        300: myColor.withOpacity(0.4),
+        400: myColor.withOpacity(0.5),
+        500: myColor.withOpacity(0.6),
+        600: myColor.withOpacity(0.7),
+        700: myColor.withOpacity(0.8),
+        800: myColor.withOpacity(0.9),
+        900: myColor.withOpacity(1.0),
+      },
+    );
+       ThemeData myTheme = ThemeData(
+      primarySwatch: myThemeColor,
+    );
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
+      title: 'Walk and win',
+      theme: myTheme,
       home:  splashScreen(),
     );
   }
