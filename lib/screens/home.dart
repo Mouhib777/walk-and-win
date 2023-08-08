33db3ebf845f12503c7c7546d9a11ea628eadb08
Mouhib777 <mouhibsahbani@gmail.com>
2023-08-08 12:33:42 -0400

    c

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_blue/flutter_blue.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +11,79 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+ FlutterBlue flutterBlue = FlutterBlue.instance;
+  BluetoothDevice? targetDevice;
+  BluetoothCharacteristic? targetCharacteristic;
+  List<int> receivedData = [];
+
+  void scanAndConnect() {
+    flutterBlue.scan().listen((scanResult) {
+      // device.name == ism device li yodhher f parametre bluetooth
+      if (scanResult.device.name == 'HM10') {
+        targetDevice = scanResult.device;
+        flutterBlue.stopScan();
+        connectToDevice();
+      }
+    });
+  }
+
+  void connectToDevice() async {
+    if (targetDevice != null) {
+      await targetDevice!.connect();
+      discoverServices();
+    }
+  }
+
+  void discoverServices() async {
+    List<BluetoothService> services = await targetDevice!.discoverServices();
+    services.forEach((service) {
+      service.characteristics.forEach((characteristic) {
+        if (characteristic.uuid.toString() ==
+        // badelha bil uuid 
+        // exemple 0000XXXX-0000-1000-8000-00805F9B34FB
+         "YOUR_CHARACTERISTIC_UUID" 
+         ) {
+          targetCharacteristic = characteristic;
+          subscribeToCharacteristic();
+        }
+      });
+    });
+  }
+
+  void subscribeToCharacteristic() async {
+    if (targetCharacteristic != null) {
+      await targetCharacteristic!.setNotifyValue(true);
+      targetCharacteristic!.value.listen((value) {
+        setState(() {
+          receivedData = value;
+        });
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bluetooth Example'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed:() {
+                 print("object");
+                scanAndConnect ; 
+               
+
+              } ,
+              child: Text('Connect to Device'),
+            ),
+            Text('Received Data: ${receivedData.isEmpty ? "None" : receivedData.join(", ")}'),
+          ],
+        ),
+      ),
+    );
   }
 }
